e06dfbb78d3bda7c8238c1e62f892839ea91d06c
Kiran <adepu.kiran547@gmail.com>
2015-03-16 17:38:25 +0530

    initial setup

---
+++ project-setup/library/js/modules/viewport.js
@@ -0,0 +1,104 @@
+
+;(function ( root, factory ) {
+
+    if ( typeof define === 'function' && define.amd ) {
+
+        // AMD. Register as an anonymous module.
+        define( factory );
+
+    } else {
+
+        // Browser global
+        root.viewport = factory();
+    }
+
+} )( this, ( function( pubsub ) {
+    
+
+    var vp = function ( input ) {
+        
+        var currentVp
+            , innerWidth
+            , oldVp
+            , check
+            , isVp
+            ;
+            
+        var settings = [
+            {
+                name: 'small',
+                width: 744
+            },
+            {
+                name: 'medium',
+                width: 924
+            },
+            {
+                name: 'large',
+                width: 1152
+            },
+            {
+                name: 'xlarge',
+                width: 40000
+            }
+        ];        
+
+        if ( typeof input === 'object' ) {
+
+            settings = input;
+
+        }
+
+        check = function() {
+
+            var item
+                , i
+                , length
+                ;
+
+            innerWidth = window.innerWidth;
+
+            for( i=0, length = settings.length; i < length; i++ ) {
+
+                item = settings[ i ];
+
+                if( item.width > innerWidth ) {
+
+                    oldVp = currentVp ? currentVp : item.name;
+                    currentVp = item.name;
+
+                    /**
+                     * publish viewport/change on viewport changes. i.e if oldVp !== currentVp
+                     */
+                    if( oldVp !== currentVp ) {
+
+                        pubsub( 'viewport/change' ).publish();
+
+                    }
+
+                    return currentVp;
+
+                }
+
+            }
+
+        };
+
+        isVp = function() {
+
+            return currentVp || check();
+
+        };
+
+        pubsub( 'window/resize' ).subscribe( check );
+
+ 
+        return {
+            is : isVp
+        };
+
+    };
+
+    return vp;
+
+})( this.pubsub) );
